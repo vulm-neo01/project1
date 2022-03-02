71b08be77f21434508cb999c9e9559384b7e0d72
vulm-neo01 <minhvu8a@gmail.com>
2022-03-02 22:26:21 +0700

    commit on master

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = function(props){
+    return(
+        <div>
+            <h1>
+                Welcome to React!
+            </h1>
+        </div>
+    )
+}
+
+export default Welcome;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import Welcome from './components/Welcome';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Welcome/>
   </React.StrictMode>,
   document.getElementById('root')
 );
